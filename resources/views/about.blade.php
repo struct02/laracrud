625688b2ccad3568bf4a03f6c1d0ad4d84a3718f
Philip Antwi <struct02>
2025-07-19 11:42:22 +0000

    route wildcards and views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>This is the about page</h1>
+    <p>{{ $greetings }}</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get('/about', function(){
 
-return view('about');
+return view('about', ["greetings"=>"Good morning"]);
 
 });
 
