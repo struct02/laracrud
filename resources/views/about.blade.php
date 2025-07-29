d7e21176589154fa3c639a7a8201051852b84de9
Philip Antwi <struct02>
2025-07-29 21:39:37 +0000

    layout created and merged in about.blade the components folder was created and layout page was created

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About us</title>
-</head>
-<body>
+
+<x-layout>
     <h1>This is the about page</h1>
     <p>{{ $greetings }}</p>
-</body>
-</html>
+</x-layout>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Crud</title>
+</head>
+<body>
+
+    <header>
+
+        <nav>
+        <a href="/about"></a>
+        <a href="/team"></a>
+        </nav>
+    </header>
+
+    <main class="container">
+        {{ $slot }}
+    </main>
+    
+</body>
+</html>
