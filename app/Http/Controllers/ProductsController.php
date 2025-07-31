9ebdec6ae1ad4e31874f22d1cc310df9b3558afb
Philip Antwi <struct02>
2025-07-31 15:39:15 +0000

    ProductsController updated to accept data from form

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class ProductsController extends Controller
 {
@@ -10,5 +11,15 @@ class ProductsController extends Controller
 
     public function addProduct(){
         return view('ourteam.create');
+    }
+
+    public function createProduct(Request $request){
+
+        $post = new Product();
+        $post->prodname = $request->prodname;
+        $post->prodq = $request->prodq;
+        $post->proddesc = $request->proddesc;
+        $post->save();
+
     }
 }
